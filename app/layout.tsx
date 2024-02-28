9c5c148199f019e328235ec9236b6b2946915a6e
mnaibei <muchajulius@gmail.com>
2024-02-28 09:33:56 +0300

    Implementing dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
+"use client";
+import { useState, useEffect } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import "@styles/globals.css";
-
-export const metadata = {
-  title: "Prompt finder",
-  description: "Find the right prompt for your writing",
-};
 
 export default function RootLayout({
   children,
@@ -14,17 +12,54 @@ export default function RootLayout({
   children: React.ReactNode;
   session: any;
 }) {
+  const [darkMode, setDarkMode] = useState(() => {
+    // Get the initial state from localStorage
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
+  useEffect(() => {
+    // Save the state to localStorage whenever it changes
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode: boolean) => !prevDarkMode);
+  };
+
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={darkMode ? "dark" : ""}>
+      <body className={darkMode ? "dark" : ""}>
         <Provider session={session}>
-          <div className="main">
-            <div className="gradient" />
+          <div>
+            {!darkMode && (
+              <div className="main">
+                <div className="gradient" />
+              </div>
+            )}
+            <main className="app relative">
+              <Nav />
+              <button
+                aria-label="Toggle dark mode"
+                className="fixed bottom-0 right-0 mb-4 mr-4 z-10 text-gray-800 dark:text-gray-200 sm:mb-8 sm:mr-8"
+                onClick={toggleDarkMode}>
+                {darkMode ? (
+                  <BsSun
+                    size={30}
+                    color="white"
+                    className="border-2 rounded-full p-1"
+                  /> // Change color to white when darkMode is true
+                ) : (
+                  <BsMoon
+                    size={30}
+                    color="black"
+                    className="border-2 border-black rounded-full p-1"
+                  /> // Change color to black when darkMode is false
+                )}
+              </button>
+              {children}
+            </main>
           </div>
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
         </Provider>
       </body>
     </html>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
 export default function Nav() {
   const { data: session } = useSession();
 
@@ -18,7 +18,7 @@ export default function Nav() {
   }, []);
 
   return (
-    <nav className="flex-between w-full mb-16 pt-3">
+    <nav className="flex-between w-full mb-16 pt-3 ">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
@@ -27,24 +27,16 @@ export default function Nav() {
           height={30}
           className="object-contain"
         />
-        <h1 className="logo_text">Prompt finder</h1>
+        <h1 className="logo_text">Prompt Finder</h1>
       </Link>
       <div className="sm:flex hidden">
         {session?.user ? (
-          <div className="flex gap-3 md:gap-5">
-            <Link href="/create-prompt" className="flex-center black_btn">
-              <button className="btn btn-primary">Create</button>
-            </Link>
-
-            <button
-              type="button"
-              onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
-                signOut()
-              }
-              className="outline_btn">
-              Sign out
-            </button>
-            <Link href="/profile" className="flex-center">
+          <div className="flex gap-3 md:gap-5 relative">
+            {" "}
+            <div
+              onClick={() => setToggleDropdown((prev) => !prev)}
+              className="flex-center cursor-pointer">
+              {" "}
               <Image
                 src={session?.user.image || "/assets/images/logo.svg"}
                 alt="profile"
@@ -52,7 +44,32 @@ export default function Nav() {
                 height={37}
                 className="rounded-full"
               />
-            </Link>
+            </div>
+            {toggleDropdown && ( // Dropdown menu
+              <div className="absolute right-0 mt-2 bg-white rounded-md dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}>
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}>
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full rounded-full border border-red-600 bg-transparent py-1.5 px-5 text-black transition-all hover:bg-red-500 hover:text-white text-center text-sm font-inter flex items-center justify-center">
+                  Sign out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
@@ -103,7 +120,7 @@ export default function Nav() {
                     setToggleDropdown(false);
                     signOut();
                   }}
-                  className="mt-5 w-full black_btn">
+                  className="mt-5 w-full rounded-full border border-red-600 bg-transparent py-1.5 px-5 text-black transition-all hover:bg-red-500 hover:text-white text-center text-sm font-inter flex items-center justify-center">
                   Sign out
                 </button>
               </div>
