b924f468b4e06021566df6e852d5998261fcec8b
mnaibei <muchajulius@gmail.com>
2024-03-02 16:52:40 +0300

    .

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,21 +1,13 @@
-import Prompt from "@models/prompt";
 import { connectToDatabase } from "@utils/database";
-import { NextRequest, NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
+import Prompt from "@models/prompt";
 
+export const dynamic = "force-dynamic";
 export const GET = async (req: any) => {
   try {
     await connectToDatabase();
-
     const prompts = await Prompt.find({}).populate("creator");
-
-    //To dynamically get the path
-    const path = req.nextUrl.searchParams.get("path") || "/";
-
-    revalidatePath(path);
-
-    return NextResponse.json(prompts);
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 });
+    return new Response(JSON.stringify(error), { status: 500 });
   }
 };
