66e376b2ed02ce7c6dd40cab2ff2b42c0f6491d7
mnaibei <muchajulius@gmail.com>
2024-03-03 11:38:46 +0300

    prevent vercel caching response

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -5,8 +5,20 @@ export const GET = async (req: any) => {
   try {
     await connectToDatabase();
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache, max-age=0, must-revalidate, no-store",
+      },
+    });
   } catch (error) {
-    return new Response(JSON.stringify(error), { status: 500 });
+    return new Response(JSON.stringify(error), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache, max-age=0, must-revalidate, no-store",
+      },
+    });
   }
 };
