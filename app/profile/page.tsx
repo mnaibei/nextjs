c2ee5310bb3c63981f10c793be3c720e81452a8c
mnaibei <muchajulius@gmail.com>
2024-04-18 11:38:47 +0300

    Add modal

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import ProfileComponent from "@components/Profile";
 import { Suspense } from "react";
+import ConfirmationModal from "@components/ConfirmModal";
 
 const Profile = () => {
   const router = useRouter();
@@ -34,21 +35,36 @@ const Profile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [postToDelete, setPostToDelete] = useState<any>(null);
   const handleDelete = async (post: any) => {
-    const hasConfirmed = confirm("Are you sure you want to delete this post?");
+    setPostToDelete(post);
+    setIsModalOpen(true);
+  };
 
-    if (hasConfirmed) {
+  const confirmDelete = async () => {
+    if (postToDelete) {
       try {
-        await fetch(`/api/prompt/${post._id.toString()}`, {
+        await fetch(`/api/prompt/${postToDelete._id.toString()}`, {
           method: "DELETE",
         });
 
-        const filteredPosts = posts.filter((p: any) => p._id !== post._id);
+        const filteredPosts = posts.filter(
+          (p: any) => p._id !== postToDelete._id
+        );
         setPosts(filteredPosts);
       } catch (error) {
         console.log(error);
       }
     }
+
+    // Close the modal
+    setIsModalOpen(false);
+  };
+
+  const cancelDelete = () => {
+    // Close the modal
+    setIsModalOpen(false);
   };
 
   return (
@@ -61,6 +77,14 @@ const Profile = () => {
           handleEdit={handleEdit}
           handleDelete={handleDelete}
         />
+        {isModalOpen && (
+          <ConfirmationModal
+            title="Delete Post"
+            message="Are you sure you want to delete this post?"
+            onConfirm={confirmDelete}
+            onCancel={cancelDelete}
+          />
+        )}
       </Suspense>
     </div>
   );
